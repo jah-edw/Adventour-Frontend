6d448bacc5dcc9167f1e58b3ee7553debe29ccc3
jah-edw <jah.edwards@hotmail.com>
2021-08-16 17:28:59 +0100

    feat: add IndividualTourScreen layout

--- client/screens/ExploreScreen.tsx
+++ client/screens/ExploreScreen.tsx
@@ -1,7 +1,7 @@
 import { StackNavigationProp } from '@react-navigation/stack';
 import React from 'react';
 
-import { SafeAreaView, Image, Dimensions,  StyleSheet, View, ImageBackground} from 'react-native';
+import { SafeAreaView, Image, Dimensions,  StyleSheet, View, ImageBackground, ImageSourcePropType} from 'react-native';
 import { TopNavigatorParamsList } from '../types';
 import { HorizontalSlider } from '../components/HorizontalSlider';
 import { InputButton } from '../components/InputButton';
@@ -12,6 +12,8 @@ import { GeneralButton } from '../components/GeneralButton';
 const { height, width } = Dimensions.get('window')
 export interface ExploreScreenProps {
     navigation: StackNavigationProp<TopNavigatorParamsList, 'ExploreScreen'>
+    source: ImageSourcePropType
+
     
 } 
 
