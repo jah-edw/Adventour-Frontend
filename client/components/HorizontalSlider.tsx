6d156a21ffe125ef745e9554d3c8db745162698a
Daniel Cassell <daniel.daniel.cc97@gmail.com>
2021-08-17 14:14:10 +0100

    style: make Tour responsive

--- client/components/HorizontalSlider.tsx
+++ client/components/HorizontalSlider.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Dimensions } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import {Tour} from './Tour'
+
 
 const arrayOfTours = [{title: 'Tour1', rating: 1, imgUrl: {uri:'https://media.istockphoto.com/photos/studio-portrait-of-military-general-in-formal-uniform-picture-id461247467?k=6&m=461247467&s=612x612&w=0&h=YhU9PtO0IJtxhpILLMVzwIncNrSS0ogU-PYRfA3ac1M='}}, {title: 'Tour2', rating: 2, imgUrl: {uri: 'https://images.unsplash.com/photo-1533929736458-ca588d08c8be?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80'}}, {title: 'Tour3', rating: 3, imgUrl: {uri: 'https://www.telegraph.co.uk/content/dam/Travel/Destinations/Europe/United%20Kingdom/London/london-aerial-thames-guide-xlarge.jpg'}}, {title: 'Tour4', rating: 4, imgUrl: {uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLZfW66pXBiv16RZTXBYG00ERIo0sS7_WBJQ&usqp=CAU'}}]
 
@@ -23,17 +24,3 @@ export const HorizontalSlider = () => {
     )
 
 }
-
-const styles = StyleSheet.create({
-    container: {
-        height: 200,
-        width:300,
-        backgroundColor:'green',
-        borderColor: 'black',
-        borderWidth:2,
-        marginLeft:30,
-        borderRadius:35,
-        shadowOffset: {width:5, height:7}, 
-        shadowOpacity: 0.2,
-    }, 
-}) 
--- client/components/Tour.tsx
+++ client/components/Tour.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Text, StyleSheet, View, Dimensions, ImageBackground } from 'react-native';
 
+const { width, height} = Dimensions.get('window');
+const ratio = width * height /1000
+
+
 const url = 'https://images.unsplash.com/photo-1533929736458-ca588d08c8be?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80'
 
 const image = { uri: "https://images.unsplash.com/photo-1533929736458-ca588d08c8be?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80" };
@@ -16,32 +20,21 @@ export const Tour = ({title, img}) => {
 }
 
 const styles = StyleSheet.create({
-    // container: {
-    //     height: 200,
-    //     width:300,
-    //     borderColor: 'black',
-    //     borderWidth:2,
-    //     marginLeft:30,
-    //     borderRadius:35,
-    //     shadowOffset: {width:5, height:7}, 
-    //     shadowOpacity: 0.2,
-    // }, 
-
     container: {
       flexDirection: 'column',
       justifyContent: 'flex-end',
-      height: 200
+      height: height/4.63
     },
     background: {
         flex: 1,
         resizeMode: 'cover',
-        height: 200,
-        width: 300,
-        marginLeft: 30
+        height: height/4.63,
+        width: width/1.426,
+        marginLeft: width/14.266
     },
     title: {
       color: 'white',
-      fontSize: 20,
-      margin: 10
+      fontSize: ratio/19.8,
+      margin: ratio/39.6
     }
 }) 
