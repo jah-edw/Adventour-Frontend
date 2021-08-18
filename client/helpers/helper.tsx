b4f922f57313e0c9087ce29e5b82843cfb79ac54
jah-edw <jah.edwards@hotmail.com>
2021-08-18 16:36:15 +0100

    fix: format navigators & helpers

--- client/helpers/helper.tsx
+++ client/helpers/helper.tsx
@@ -1,16 +1,16 @@
 import { Dimensions } from "react-native";
 
 interface DimensionProps {
-    ratio: number;
-    width: number;
-    height: number;
+  ratio: number;
+  width: number;
+  height: number;
 }
 
 export const getWindow = (): DimensionProps => {
-    const { width, height } = Dimensions.get('window');
-    return {
-        ratio: width * height /1000,
-        height, 
-        width
-    }
-}
+  const { width, height } = Dimensions.get("window");
+  return {
+    ratio: (width * height) / 1000,
+    height,
+    width,
+  };
+};
--- client/navigation/TabNavigator.tsx
+++ client/navigation/TabNavigator.tsx
@@ -1,37 +1,38 @@
-import React from 'react';
-
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { SimpleLineIcons, Foundation } from '@expo/vector-icons';
-
-import GameScreen from '../screens/GameScreen';
-import NotebookScreen from '../screens/NotebookScreen';
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { SimpleLineIcons, Foundation } from "@expo/vector-icons";
+import GameScreen from "../screens/GameScreen";
+import NotebookScreen from "../screens/NotebookScreen";
 
 const Tabs = createBottomTabNavigator();
 
-export const Game: React.FC = () => {
-    return (
-        <Tabs.Navigator>
-            <Tabs.Screen 
-            name="Game" 
-            component={GameScreen} 
-            options={{ 
-                headerShown: false,
-                tabBarLabel: 'Game',
-                tabBarIcon: () => (
-                <Foundation name="magnifying-glass" size={24} color="black" />
-                ) 
-            }} />
-            <Tabs.Screen 
-            name="Notebook" 
-            component={NotebookScreen} 
-            options={{ 
-                headerShown: false,
-            tabBarLabel: 'Notebook',
-            tabBarIcon: () => (
-            <SimpleLineIcons name="notebook" size={24} color="black" />
+interface Props {}
 
-        ) }} 
-            />
-        </Tabs.Navigator>
-    )
-}
+export const Game: React.FC<Props> = () => {
+  return (
+    <Tabs.Navigator>
+      <Tabs.Screen
+        name="Game"
+        component={GameScreen}
+        options={{
+          headerShown: false,
+          tabBarLabel: "Game",
+          tabBarIcon: () => (
+            <Foundation name="magnifying-glass" size={24} color="black" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Notebook"
+        component={NotebookScreen}
+        options={{
+          headerShown: false,
+          tabBarLabel: "Notebook",
+          tabBarIcon: () => (
+            <SimpleLineIcons name="notebook" size={24} color="black" />
+          ),
+        }}
+      />
+    </Tabs.Navigator>
+  );
+};
